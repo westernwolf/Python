5b90b7333f2ebef8ffddca510a31b317856ed498
Andreas Senn <andreassenn@gmx.ch>
2020-03-01 17:34:20 +0100

    finished VL1

--- SubTex/Code/DatenTypen.py
+++ SubTex/Code/DatenTypen.py
@@ -37,6 +37,49 @@ set2 = frozenset(set1)
 steckbrief = {
         "Vorname": "Pippi",
         "Nachname": "Langstrumpf",
-        "Alter" : 9,
+        "Alter": 9,
         "Hobbies": ["Reiten", "Spielen"],
 }
+
+n = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+primes = {2, 3, 4, 7}
+
+primes.add(11)  # Element wird hinzugefuegt (solange nicht vorhanden)
+# .add veraendert den set
+
+x = n.intersection(primes)
+# gibt set zurueck mit Elementen, die in beiden sets vorkommen
+
+n.intersection_update(primes)
+# Vergleichbar mit:
+# y = n.intersection(primes)
+# n = y
+
+y = n.difference(primes)
+# gibt set zurueck mit Elementen, die nur in "n" vorkommt
+
+n.difference_update(primes)
+# Vergleichbar mit:
+# y = n.difference(primes)
+# n = y
+
+buch = {
+        "Titel": "Call of the wild",
+        "Autor": "J.R.R.Tolkien",
+        "ISBN": 112358132144,
+        "Preis": "22.50 CHF",
+}
+
+buch["Erscheinungsjahr"] = 1980  # wird ueberschrieben
+print(buch["Titel"])
+
+zahl = buch.get("Seitenzahl", 0)
+
+zahl1 = input("1. Zahl: ")
+
+start = 0
+stop = 6
+step = 1
+range(stop) == range(0, stop, 1)
+range(start, stop) == range(start, stop, 1)
+range(start, stop, step)
